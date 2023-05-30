eafd06017cfb39bf06897928dd52b09a13ab3b6b
Mohammad Reza SharifKhani <sharifkhani.mr@gmail.com>
2023-05-30 17:34:40 +0430

    add project

---
+++ app/Illustration/page.tsx
@@ -0,0 +1,51 @@
+import type { Metadata } from 'next';
+import {
+  GitHubIcon,
+  YoutubeIcon,
+  ArrowIcon,
+  TwitterIcon,
+} from 'components/icons';
+
+export const metadata: Metadata = {
+  title: 'Illustration',
+  description: 'VP of Developer Experience at Vercel.',
+};
+
+export default function IllustrationPage() {
+  return (
+    <section>
+      <h1 className="font-bold text-3xl font-serif">Illustration</h1>
+      <p className="my-5 text-neutral-800 dark:text-neutral-200">
+        Hey, I'm Lee. Most folks know me as <b>leerob</b> online.
+      </p>
+      <div className="prose prose-neutral dark:prose-invert text-neutral-800 dark:text-neutral-200">
+        <p>
+          I'm currently the <b>VP of Developer Experience at Vercel</b>, where I
+          lead our Developer Relations and Documentation teams. I focus on{' '}
+          <b>educating and growing</b> the Vercel and Next.js communities.
+        </p>
+        <hr />
+        <p>
+          I'm passionate about many creative pursuits, including music,
+          photography, videography, and of course, coding. This combination of
+          interests is what ultimately led me to my current role in building
+          developer communities.
+        </p>
+        <p>
+          I <b>love</b> building for the web. From something as simple as a
+          single HTML file – all the way to large Next.js applications. The web
+          is incredible. Anyone can become a developer, writer, or creator – and
+          no one has to ask for permission. You can just build.
+        </p>
+        <p className="mb-8">
+          Outside of Vercel, I <b>angel invest</b> in developer tools companies
+          and <b>advise early-stage startups</b>. I also do Developer Relations
+          consulting work, helping companies take their DevRel function from 0
+          to 1, or provide guidance on growing communities, content creation,
+          and developer marketing.
+        </p>
+       
+      </div>
+    </section>
+  );
+}
---
+++ components/sidebar.tsx
@@ -0,0 +1,182 @@
+"use client";
+
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { LayoutGroup, motion } from "framer-motion";
+
+const navItems = {
+  "/": {
+    name: "home",
+    subs: {},
+  },
+  "/about": {
+    name: "about",
+    subs: {},
+  },
+  "/Illustration": {
+    name: "Illustration",
+    subs: [
+      { url: "/Illustration/book", name: "book" },
+      { url: "/Illustration/practical", name: "practical" },
+    ],
+  },
+  "/GraphicDesign": {
+    name: "Graphic Design",
+    subs: {},
+  },
+};
+
+function Logo() {
+  return (
+    <Link aria-label="Lee Robinson" href="/">
+      <motion.svg
+        className="text-black dark:text-white h-[25px] md:h-[37px]"
+        width="55"
+        height="55"
+        viewBox="0 0 512 512"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <motion.path
+          initial={{ x: -200, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{
+            duration: 0.5,
+            type: "spring",
+            stiffness: 50,
+          }}
+          d="M71.5 142.3c.6-5.9-1.7-11.8-6.1-15.8L20.3 72.1V64h140.2l108.4 237.7L364.2 64h133.7v8.1l-38.6 37c-3.3 2.5-5 6.7-4.3 10.8v272c-.7 4.1 1 8.3 4.3 10.8l37.7 37v8.1H307.3v-8.1l39.1-37.9c3.8-3.8 3.8-5 3.8-10.8V171.2L241.5 447.1h-14.7L100.4 171.2v184.9c-1.1 7.8 1.5 15.6 7 21.2l50.8 61.6v8.1h-144v-8L65 377.3c5.4-5.6 7.9-13.5 6.5-21.2V142.3z"
+          fill="currentColor"
+        />
+      </motion.svg>
+    </Link>
+  );
+}
+
+export default function Navbar() {
+  let pathname = usePathname() || "/";
+  if (pathname.includes("/blog/")) {
+    pathname = "/blog";
+  }
+
+  return (
+    <aside className="md:w-[200px] md:flex-shrink-0 -mx-4 md:mx-0 md:px-0 font-serif">
+      <div className="lg:sticky lg:top-20">
+        <div className="ml-2 md:ml-[12px] mb-2 px-4 md:px-0 md:mb-8 space-y-10 flex flex-col md:flex-row items-start ">
+          <Logo />
+        </div>
+        <LayoutGroup>
+          <nav
+            className="flex flex-row md:flex-col items-start relative px-4 md:px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+            id="nav"
+          >
+            <div className="flex flex-row md:flex-col space-x-0 pr-10 mb-2 mt-2 md:mt-0">
+              {Object.entries(navItems).map(([path, { name, subs }]) => {
+                const isActive = path === pathname;
+                if (subs.length) {
+                  return (
+                    <>
+                      <Link
+                        key={path}
+                        href={path}
+                        className={clsx(
+                          "transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle ",
+                          {
+                            "text-neutral-500": !isActive,
+                            "font-bold": isActive,
+                          }
+                        )}
+                      >
+                        <span className="relative py-[5px] px-[10px] ">
+                          {name}
+                          {path === pathname ? (
+                            <motion.div
+                              className="absolute inset-0 bg-neutral-100 dark:bg-neutral-800 rounded-md z-[-1]"
+                              layoutId="sidebar"
+                              transition={{
+                                type: "spring",
+                                stiffness: 350,
+                                damping: 30,
+                              }}
+                            />
+                          ) : null}
+                        </span>
+                      </Link>
+
+                      <>
+                        {subs.map((item) => {
+                          return (
+                            <>
+                              <Link
+                                key={item.url}
+                                href={item.url}
+                                className={clsx(
+                                  "transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle",
+                                  {
+                                    "text-neutral-500": !isActive,
+                                    "font-bold": isActive,
+                                  }
+                                )}
+                              >
+                                <span className="relative py-[5px] px-[10px] ml-5">
+                                  {item.name}
+                                  {item.url === pathname ? (
+                                    <motion.div
+                                      className="absolute inset-0 bg-neutral-100 dark:bg-neutral-800 rounded-md z-[-1]"
+                                      layoutId="sidebar"
+                                      transition={{
+                                        type: "spring",
+                                        stiffness: 350,
+                                        damping: 30,
+                                      }}
+                                    />
+                                  ) : null}
+                                </span>
+                              </Link>
+                            </>
+                          );
+                        })}
+                      </>
+                    </>
+                  );
+                } else {
+                  return (
+                    <>
+                      <Link
+                        key={path}
+                        href={path}
+                        className={clsx(
+                          "transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle",
+                          {
+                            "text-neutral-500": !isActive,
+                            "font-bold": isActive,
+                          }
+                        )}
+                      >
+                        <span className="relative py-[5px] px-[10px]">
+                          {name}
+                          {path === pathname ? (
+                            <motion.div
+                              className="absolute inset-0 bg-neutral-100 dark:bg-neutral-800 rounded-md z-[-1]"
+                              layoutId="sidebar"
+                              transition={{
+                                type: "spring",
+                                stiffness: 350,
+                                damping: 30,
+                              }}
+                            />
+                          ) : null}
+                        </span>
+                      </Link>
+                    </>
+                  );
+                }
+              })}
+            </div>
+          </nav>
+        </LayoutGroup>
+      </div>
+    </aside>
+  );
+}
