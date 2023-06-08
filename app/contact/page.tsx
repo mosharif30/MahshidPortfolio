106de53b14612ed5d6a4849b2f7e150ac6ca779d
Mohammad Reza SharifKhani <sharifkhani.mr@gmail.com>
2023-06-08 14:22:59 +0430

    add face book and email to contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -23,6 +23,7 @@ export default function AboutPage() {
 
   const onSubmit: SubmitHandler<FormValues> = (data) => {
     console.log(data);
+
     // axios
     //   .post("https://formbold.com/s/oJpPE", data)
     //   .then(function (response) {
@@ -34,59 +35,69 @@ export default function AboutPage() {
   };
   return (
     <section className="w-full md:w-1/2 bg-white ml-auto h-full justify-center">
-      <div className="container mx-auto">
-        <div className="max-w-md mx-auto my-10 bg-white p-5 ">
+      <div className="container mx-auto h-full mt-auto">
+        <div className="max-w-lg mx-auto my-10 bg-white p-5 ">
+          <div className="flex justify-between mb-10">
+            <span className="inline-block  rounded-md text-xl  text-customBlue  ">
+              Instagram
+            </span>
+            <span className="inline-block  rounded-md text-xl  text-customGray  ">
+              FaceBook
+            </span>
+          </div>
           <h2 className="text-2xl font-bold mb-5">Contact Me</h2>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <div className="mb-4">
-              <label
-                htmlFor="name"
-                className="block text-gray-700 font-semibold mb-1"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                id="name"
-                className={`w-full px-4 py-2 rounded-md border ${
-                  errors.name ? "border-red-500" : "border-gray-300"
-                }`}
-                placeholder="Enter your name"
-                {...register("name", { required: "Name is required" })}
-              />
-              {errors.name && (
-                <span className="text-red-500 text-sm">
-                  {errors.name.message}
-                </span>
-              )}
-            </div>
-            <div className="mb-4">
-              <label
-                htmlFor="email"
-                className="block text-gray-700 font-semibold mb-1"
-              >
-                Email
-              </label>
-              <input
-                type="email"
-                id="email"
-                className={`w-full px-4 py-2 rounded-md border ${
-                  errors.email ? "border-red-500" : "border-gray-300"
-                }`}
-                placeholder="Enter your email"
-                {...register("email", {
-                  required: "Email is required",
-                  pattern: {
-                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                    message: "Invalid email address",
-                  },
-                })}
-              />
-              {errors.email && (
-                <span className="text-red-500 text-sm">
-                  {errors.email.message}
-                </span>
-              )}
+            <div className="flex flex-row justify-between">
+              <div className="mb-4">
+                <label
+                  htmlFor="name"
+                  className="block text-gray-700 font-semibold mb-1"
+                >
+                  Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  className={`w-full px-4 py-2 rounded-md border-b-2 ${
+                    errors.name ? "border-red-500" : "border-gray-300"
+                  }`}
+                  placeholder="Enter your name"
+                  {...register("name", { required: "Name is required" })}
+                />
+                {errors.name && (
+                  <span className="text-red-500 text-sm">
+                    {errors.name.message}
+                  </span>
+                )}
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="email"
+                  className="block text-gray-700 font-semibold mb-1"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  className={`w-full px-4 py-2 rounded-md border-b-2  ${
+                    errors.email ? "border-red-500" : "border-gray-300"
+                  }`}
+                  placeholder="Enter your email"
+                  {...register("email", {
+                    required: "Email is required",
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                      message: "Invalid email address",
+                    },
+                  })}
+                />
+                {errors.email && (
+                  <span className="text-red-500 text-sm">
+                    {errors.email.message}
+                  </span>
+                )}
+              </div>
             </div>
             <div className="mb-4">
               <label
@@ -97,10 +108,10 @@ export default function AboutPage() {
               </label>
               <textarea
                 id="message"
-                className={`w-full px-4 py-2 rounded-md border ${
+                className={`w-full px-4 py-2 rounded-md border-b-2  ${
                   errors.message ? "border-red-500" : "border-gray-300"
                 }`}
-                rows={4}
+                rows={2}
                 placeholder="Enter your message"
                 {...register("message", { required: "Message is required" })}
               />
@@ -118,6 +129,9 @@ export default function AboutPage() {
               Submit
             </button>
           </form>
+          <span className="inline-block  rounded-md text-3xl  text-customBlue mt-10 ">
+            mahshidasoudekhah@gmail.com
+          </span>
         </div>
       </div>
     </section>
--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -1,5 +1,4 @@
 import clsx from "clsx";
-import { motion } from "framer-motion";
 import Link from "next/link";
 import React from "react";
 
@@ -9,10 +8,9 @@ function NavItem({ path, isActive, setIsMenuOpen, pathname, name }) {
       key={path}
       href={path}
       className={clsx(
-        "transition-all  hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle text-4xl",
+        "transition-all text-white  hover:text-gray-200 dark:hover:text-neutral-200 flex align-middle text-4xl",
         {
-          "text-white": !isActive,
-          "font-bold": isActive,
+          " line-through": isActive,
         }
       )}
     >
@@ -23,17 +21,6 @@ function NavItem({ path, isActive, setIsMenuOpen, pathname, name }) {
         className="relative py-[20px] px-[10px] my-auto "
       >
         {name}
-        {path === pathname ? (
-          <motion.div
-            className="absolute inset-0 bg-neutral-100 dark:bg-neutral-800 rounded-md z-[-1]"
-            layoutId="sidebar"
-            transition={{
-              type: "spring",
-              stiffness: 350,
-              damping: 30,
-            }}
-          />
-        ) : null}
       </span>
     </Link>
   );
